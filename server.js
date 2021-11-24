c18d6ece29f374d4b8d00a45747ebc7b528a118b
Bryan Kimani <brnkimani@gmail.com>
2021-11-24 08:23:00 +0300

    Install multer plugin & get the uploaded file info

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -9,6 +13,14 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 
